af076bf6d501077af61ea449fd17d5140dbd5b45
kiringe <kiringe0@mail.ru>
2024-05-24 17:23:02 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var menuItems = document.querySelectorAll(".menu-item");
+    menuItems.forEach(function(item) {
+      item.addEventListener("click", function() {
+        menuItems.forEach(function(item) {
+          item.classList.remove("active");
+        });
+        this.classList.add("active");
+      });
+    });
+});
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelector('.slides');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const dots = document.querySelectorAll('.dot');
+let currentSlide = 0;
+prevBtn.addEventListener('click', () => {
+    currentSlide--;
+    if (currentSlide < 0) {
+    currentSlide = slides.children.length - 1;
+    }
+    updateSlider();
+});
+nextBtn.addEventListener('click', () => {
+    currentSlide++;
+    if (currentSlide === slides.children.length) {
+    currentSlide = 0; 
+    }
+    updateSlider();
+});
+dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+    currentSlide = i;
+    updateSlider();
+    });
+});
+function updateSlider() {
+    slides.style.transform = `translateX(-${currentSlide * (100 / slides.children.length)}%)`;
+    updateDots();
+}
+function updateDots() {
+    dots.forEach((dot) => dot.classList.remove('active'));
+    dots[currentSlide].classList.add('active');
+}
+function autoplay() {
+    currentSlide++;
+    if (currentSlide === slides.children.length) {
+    currentSlide = 0;
+    }
+    updateSlider();
+}
+slides.style.transition = "transform 0.5s ease-in-out";
+setInterval(autoplay, 4000);
+updateSlider();
